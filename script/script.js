eef28ac3d81670825378708f69456cecd24492f0
Gaya72 <gayane.grig72@gmail.com>
2024-11-27 16:40:39 +0100

    plouf

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
-
-
-
 var myCarousel = document.querySelector('#carouselExampleCaptions');
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 3000, // 3 վայրկյան
   ride: 'carousel' // Սկսել ավտոմատ
 });
+
+document.addEventListener('scroll', function() {
+  const parallaxElements = document.querySelectorAll('.parallax');
+  parallaxElements.forEach(element => {
+      let offset = window.pageYOffset;
+      element.style.backgroundPositionY = offset * 0.5 + "px";
+  });
+});
+
 
 
 
